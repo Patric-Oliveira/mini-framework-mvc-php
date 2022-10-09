12801b41de36769705595d99a57bfe5170e131ef
Patric Oliveira <patric.eng.computation@gmail.com>
2022-10-09 15:47:15 -0300

    Script Core - Parte 3

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -6,7 +6,12 @@ class Core
 {
     private $explodeURI;
     private $area;
-
+    
+    /**
+     * Método construtor que inicializa as funções so core
+     *
+     * @return void
+     */
     public function __construct()
     {
         // Quebra a string nas barras e converte para um array 
@@ -21,20 +26,23 @@ class Core
         // Reordena o array novamente
         $this->explodeURI = array_values($this->explodeURI);
 
+        // Obtém a área atual
         $this->area = $this->getArea();
 
-        echo $this->area;
-        dd($this->explodeURI);
+        $this->execute();
     }
-
+    
+    /**
+     * Valida se existe parâmetros e retorna a área correta a ser carregada
+     *
+     * @return string Retorna a área do site, por padrão retorna a Home
+     */
     private function getArea() : string
     {
         if (!isset($this->explodeURI[0]) || !is_array($this->explodeURI) || count($this->explodeURI) == 0) {
 
             return 'Home';
         }
-
-        $area = $this->explodeURI[0];
 
         foreach (AREAS as $area => $value) {
 
@@ -49,6 +57,57 @@ class Core
 
     private function execute()
     {
+        $controller = $this->getController();
+        $method = $this->getMethod($controller);
 
+        //$this->getParams()
+
+    }
+    
+    /**
+     * Retorna o controller a ser ultilizado com base na URI
+     *
+     * @return string Retorna a HomeController caso a controller informada não exista 
+     */
+    private function getController() : string
+    {
+        $contBase = 'App\\Src\\' . $this->area . '\\Controller\\';
+
+        if (!isset($this->explodeURI[0])) {
+            return $contBase . 'HomeController';
+        }
+
+        $tmpController = $contBase . ucfirst($this->explodeURI[0] . 'Controller');
+
+        if (!class_exists($tmpController)) {
+            return $contBase . 'HomeController';
+        }
+        return $tmpController;
+        
+    }
+
+    private function getMethod($controller) : string
+    {
+        if (!isset($this->explodeURI[1])) {
+            return 'index';
+        }
+
+        if (!method_exists($controller, $this->explodeURI[1])) {
+            return 'index';
+        }
+        return $this->explodeURI[1];
+    }
+
+    private function getParams() : array
+    {
+        if (!isset($this->explodeURI[2])) {
+            return [];
+        }
+        $tmpParams = [];
+
+        for ($i= 2; $i < count($this->explodeURI); $i++) { 
+            $tmpParams[] = $this->explodeURI[$i];
+        }
+        return $tmpParams;
     }
 }
---
+++ app/Src/Home/Controller/ProdutoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Src\Home\Controller;
+
+class ProdutoController
+{
+    public function __construct()
+    {
+        echo 'Construtor Produto - Main';
+    }
+
+    public function index()
+    {
+        echo 'Index Produto - Main';
+    }
+
+    public function listar()
+    {
+        echo 'Other Produto - Main';
+    }
+}
