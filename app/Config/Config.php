d0c1798af7af4394823ad55a228a04c899d7861b
Patric Oliveira <patric.eng.computation@gmail.com>
2022-10-09 10:28:22 -0300

    Composer Autoload e Dependências

---
+++ app/Config/Config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('BASE', '/mini-framework-mvc-php/');
+define('HOST', 'http://localhost/mini-framework-mvc-php/');
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('../vendor/autoload.php');
+
+use App\Core\Controller;
+
+$controller = new Controller();
+
+dd(BASE);
